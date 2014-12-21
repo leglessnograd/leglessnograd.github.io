88a93578bfe89e8edce0e3fe23ae1690da22f5da
Charlie Knight <leglessnograd@gmail.com>
2014-12-22 00:21:00 +1100

    I suck at repomaking

--- repo/import.sh
+++ repo/import.sh
@@ -1,2 +1,7 @@
 #!/bin/bash 
-dpkg-scanpackages ./ /dev/null |gzip > Packages.gz
+
+dpkg-scanpackages  . /dev/null  > Packages
+
+gzip -f -k Packages > Packages.gz
+bzip2 -k -f Packages > Packages.bz2
+
